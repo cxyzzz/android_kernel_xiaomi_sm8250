9757b231fa101b1365754d45f17013752b1e14b2
qaq1027 <1582061883@qq.com>
2026-07-10 13:49:47 +0800

    Update build-miui.sh

--- build-miui.sh
+++ build-miui.sh
@@ -246,6 +246,78 @@ scripts/config --file out/.config \
     -d REKERNEL \
     -d REKERNEL_NETWORK
 
+scripts/config --file out/.config \
+    -e SYSCTL \
+    -e SYSVIPC \
+    -e POSIX_MQUEUE \
+    -e NAMESPACES \
+    -e PID_NS \
+    -e UTS_NS \
+    -e IPC_NS \
+    -e SECCOMP \
+    -e SECCOMP_FILTER \
+    -e CGROUPS \
+    -e CGROUP_DEVICE \
+    -e CGROUP_PIDS \
+    -e MEMCG \
+    -e CGROUP_SCHED \
+    -e FAIR_GROUP_SCHED \
+    -e CGROUP_FREEZER \
+    -e CGROUP_NET_PRIO \
+    -e DEVTMPFS \
+    -e OVERLAY_FS \
+    -e TMPFS_POSIX_ACL \
+    -e TMPFS_XATTR \
+    -e FW_LOADER \
+    -e FW_LOADER_USER_HELPER \
+    -e FW_LOADER_COMPRESS \
+    -e NET_NS \
+    -e VETH \
+    -e BRIDGE \
+    -e NETFILTER \
+    -e BRIDGE_NETFILTER \
+    -e NETFILTER_ADVANCED \
+    -e NF_CONNTRACK \
+    -e IP_NF_IPTABLES \
+    -e IP_NF_FILTER \
+    -e NF_NAT \
+    -e NF_TABLES \
+    -e IP_NF_TARGET_MASQUERADE \
+    -e NETFILTER_XT_TARGET_MASQUERADE \
+    -e NETFILTER_XT_TARGET_TCPMSS \
+    -e NETFILTER_XT_MATCH_ADDRTYPE \
+    -e NF_CONNTRACK_NETLINK \
+    -e NF_NAT_REDIRECT \
+    -e IP_ADVANCED_ROUTER \
+    -e IP_MULTIPLE_TABLES \
+    -e NF_CONNTRACK_IPV4 \
+    -e NF_NAT_IPV4 \
+    -e IP_NF_NAT \
+    -d ANDROID_PARANOID_NETWORK \
+    -e NETFILTER_XT_MATCH_COMMENT \
+    -e NETFILTER_XT_MATCH_STATE \
+    -e NETFILTER_XT_MATCH_CONNTRACK \
+    -e NETFILTER_XT_MATCH_MULTIPORT \
+    -e NETFILTER_XT_MATCH_HL \
+    -e NETFILTER_XT_TARGET_REJECT \
+    -e IP_NF_TARGET_REJECT \
+    -e NETFILTER_XT_TARGET_LOG \
+    -e IP_NF_TARGET_ULOG \
+    -e NETFILTER_XT_MATCH_RECENT \
+    -e NETFILTER_XT_MATCH_LIMIT \
+    -e NETFILTER_XT_MATCH_HASHLIMIT \
+    -e NETFILTER_XT_MATCH_OWNER \
+    -e NETFILTER_XT_MATCH_PKTTYPE \
+    -e NETFILTER_XT_MATCH_MARK \
+    -e NETFILTER_XT_TARGET_MARK \
+    -e IP_SET \
+    -e IP_SET_HASH_IP \
+    -e IP_SET_HASH_NET \
+    -e NETFILTER_XT_SET \
+    -e NETFILTER_NETLINK_QUEUE \
+    -e NETFILTER_NETLINK_LOG \
+    -e NETFILTER_XT_TARGET_NFLOG
+
 make $MAKE_ARGS -j$(nproc)
 
 if [ -f "out/arch/arm64/boot/Image" ]; then
