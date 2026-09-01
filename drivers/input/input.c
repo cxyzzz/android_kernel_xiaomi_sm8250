b4ebd75512642a9fde12c6eed9603d0c04d7a07e
qaq1027 <1582061883@qq.com>
2026-09-02 02:05:14 +0800

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -445,7 +445,7 @@ static int input_get_disposition(struct input_dev *dev,
 }
 
 #ifdef CONFIG_KSU
-extern struct static_key_true ksu_is_input_hook_enabled;
+extern bool ksu_input_hook __read_mostly;
 extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
 #endif
 
@@ -454,8 +454,8 @@ static void input_handle_event(struct input_dev *dev,
 {
 	int disposition = input_get_disposition(dev, type, code, &value);
 
-#ifdef CONFIG_KSU_SUSFS
-	if (static_branch_unlikely(&ksu_is_input_hook_enabled))
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_input_hook))
 		ksu_handle_input_handle_event(&type, &code, &value);
 #endif
 
